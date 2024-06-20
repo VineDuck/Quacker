315002d0abf412609c14b999251ba8c899aa3c74
Jimbo5431 <githubjimbo@bend.me.uk>
2024-06-20 16:21:05 +0100

    Attempt to skip caching.

--- RFY_AFA.user.js
+++ RFY_AFA.user.js
@@ -3,7 +3,7 @@
 // @match      https://www.amazon.co.uk/vine/vine-items?queue=potluck*
 // @match      https://www.amazon.co.uk/vine/vine-items?queue=last_chance*
 // @grant      GM.notification
-// @version    1.04
+// @version    1.05
 // ==/UserScript==
 
 // Refreshes the vine pages every 3 to 8 seconds, while page is not focused.
@@ -112,7 +112,10 @@ function checkNew() {
 
 function refreshMe() {
     console.log('Reloading!');
-    location.reload();
+    let ts = Math.floor(Date.now() / 1000);
+    let href = new URL(window.location.href);
+    href.searchParams.set('ts', ts);
+    window.location.assign(href.toString());
 }
 
 function showNotif() {
